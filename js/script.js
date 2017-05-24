d070d462d00033848da27254c027e4eedac26cf5
Yedrup <purdey.chambraud@gmail.com>
2017-05-24 23:43:23 +0200

    settings for message sent to players, creating conditions

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 (function () {
     "use strict";
     //targets
+    const elemGamePannel = document.querySelector('.GamePannel');
+    const elemColorToFind = document.querySelector('.colorToFindDiv');
+    const elemColorCreatedByPlayer = document.querySelector('.colorCreatedByPlayer');
     const redInput = document.querySelector('#red');
     const greenInput = document.querySelector('#green');
     const blueInput = document.querySelector('#blue');
-    const inputs = document.querySelectorAll('input');
+    const elemColorPannelPlayer = document.querySelector('.colorPannelPlayer');
+    const elemMessageRed = document.querySelector('.messageColorRedP');
+    const elemMessageGreen = document.querySelector('.messageColorGreenP');
+    const elemMessageBlue = document.querySelector('.messageColorBlueP');
+    const elemEndMessage = document.querySelector('.messageEndGame');
     const buttonTry = document.querySelector('#buttonTry');
     const buttonStart = document.querySelector('#buttonStart');
-    const elemColorCreatedByPlayer = document.querySelector('.colorCreatedByPlayer');
-    const elemColorToFind = document.querySelector('.colorToFindDiv');
-    const elemGamePannel = document.querySelector('.GamePannel');
+
     //values player
     var redValue;
     var greenValue;
     var blueValue;
     var playerRgb;
-     //values Random
+    //values Random
     var greenRandom;
     var blueRandom;
     var randomRgb;
@@ -23,7 +28,10 @@
     //Arays
     var colorsPlayer = [];
     var colorsRandom = [];
-
+    var colorsMessages = [];
+    var i = 0;
+    //message
+    var message = "";
     function createRandomRgb() {
         redRandom = Math.floor(256 * Math.random());
         greenRandom = Math.floor(256 * Math.random());
@@ -31,28 +39,42 @@
         randomRgb = 'rgb(' + redRandom + ',' + greenRandom + ',' + blueRandom + ')';
         elemColorToFind.style.backgroundColor = randomRgb;
     }
+    function addMessageToPlayer() {
+            colorsMessages[i].innerHTML = message;
 
+    }
     function comparison(colorsRandom, colorsPlayer) {
+        message = "";
+        colorsMessages = [elemMessageRed, elemMessageGreen, elemMessageBlue];
         colorsPlayer = [redValue, greenValue, blueValue];
         colorsRandom = [redRandom, greenRandom, blueRandom];
         var colorFound = 0;
-        var color = ['red','green','blue'];
-        for (var i = 0; i < colorsRandom.length; i++) {
+        var color = ['red', 'green', 'blue'];
+        for (i = 0; i < colorsRandom.length; i++) {
             if (colorsRandom[i] === colorsPlayer[i]) {
-                console.log("you've find the " + color[i]);
+                message = "You've find the " + color[i];
+                colorsMessages[i].className += " messageSuccess";        
                 colorFound ++;
-                console.log(colorFound);
-                if (colorFound === colorsRandom.length) {
-                    console.log("GREAT GAME!");
+            } else if (colorsRandom[i] > colorsPlayer[i]) {
+                message = ' There is not enough of ' + color[i];
+                colorsMessages[i].className += " messageFail";
+
+            } else if(colorsRandom[i] < colorsPlayer[i]) {
+                message = ' There is too much of ' + color[i];
+                colorsMessages[i].className += " messageFail";
+
+            } else if (colorsPlayer[i] > 255) {
+                message = " Remember, the maximum of each color is 255.<br>";
+                colorsMessages[i].className += " messageFail";
+            }
+             if (colorFound === colorsRandom.length) {
+                    elemEndMessage.innerHTML = " GREAT GAME! ";
                     return;
                 }
-            } else if (colorsRandom[i] > colorsPlayer[i]) {
-                console.log('There is not enough of ' + color[i]); 
-            } else if(colorsRandom[i] < colorsPlayer[i]) {
-                console.log('There is too much of ' + color[i]); 
-            } else if (colorsPlayer[i] > 255) {
-                console.log("Remember, the maximum of each color is 255");
-            } 
+                addMessageToPlayer(); 
+
+                console.log(colorsMessages[i]);
+
         }
     }
 
@@ -61,6 +83,8 @@
         createRandomRgb();
         console.log(randomRgb);
         buttonTry.addEventListener('click', () => {
+
+            elemColorPannelPlayer.style.display = "flex";
             //recuperer input
             redValue = Number(redInput.value);
             greenValue = Number(greenInput.value);
@@ -69,7 +93,6 @@
             //
             elemColorCreatedByPlayer.style.backgroundColor = playerRgb;
             comparison();
-        //count ++
         });
     });
 }());
